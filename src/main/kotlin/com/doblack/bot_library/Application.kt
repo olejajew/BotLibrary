46913c3de12a1c18234c573b0204fc8f2a6239fc
olejajew <oleg-s32@yandex.ru>
2022-03-13 17:21:46 +0300

    release 1.1.3 Bug Fixes

---
+++ src/main/kotlin/com/doblack/bot_library/Application.kt
@@ -0,0 +1,12 @@
+package com.doblack.bot_library
+
+class Application {
+
+    companion object{
+        @JvmStatic
+        fun main(args: Array<String>){
+            println("Hello world")
+        }
+    }
+
+}
