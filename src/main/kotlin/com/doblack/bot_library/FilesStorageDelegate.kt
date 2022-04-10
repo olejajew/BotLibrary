7be51cfb34feee3811f8990dddb177ed5bec31c2
olejajew <oleg-s32@yandex.ru>
2022-04-10 14:56:00 +0300

    Remove useless methods from FilesStorageDelegate.kt

--- src/main/kotlin/com/doblack/bot_library/FilesStorageDelegate.kt
+++ src/main/kotlin/com/doblack/bot_library/FilesStorageDelegate.kt
@@ -4,17 +4,9 @@ import java.io.*
 
 interface FilesStorageDelegate {
 
-    fun saveTextFile(content: String, fileName: String,  botId: String)
-
     fun getTextFileContent(fileName: String): String?
 
-    fun saveImage(imageInBase64: String, fileName: String, ): Boolean
-
-    fun deleteFiles(images: Array<String>, )
-
-    fun getImageLink( imageId: String): String
-
-    fun getFilesList(): List<String>
+    fun getImageLink(imageId: String): String
 
     fun getImageInputStream(imageName: String): InputStream
 
